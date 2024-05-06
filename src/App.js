98c9f4c7d67f54cb04d9d6361f91de2c431611cb
Julie Napoli <njulie818@gmail.com>
2024-05-06 18:02:39 +0200

    feat(routes): Ajout d'une retour de détail FRONT-6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Welcome from './views/welcome/Welcome';
 import Footer from './components/footer/Footer';
+import ImagePicker from './components/cards/details/ImagePicker';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/painting/:id" element={<ImagePicker />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
