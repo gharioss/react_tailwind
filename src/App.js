1814dd16c3f5d033c9f3fc72ce44e8686e4b4d4e
Julie Napoli <njulie818@gmail.com>
2024-05-28 23:29:03 +0200

    feat(toggleCart): création component Création du component d'affichage des objets réservés. FRONT-6

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ import Welcome from './views/welcome/Welcome';
 import Footer from './components/footer/Footer';
 import ImagePicker from './components/cards/details/ImagePicker';
 import Register from './components/Register';
+import Cart from './components/Cart';
+import { useState } from 'react';
+import { Transition } from '@headlessui/react';
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const toggleCart = () => {
+    setShowCart(prevShowCart => !prevShowCart);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar toggleCart={toggleCart} />
+      {showCart && (
+          <Transition
+          show={showCart}
+          enter="transition-opacity duration-75"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          {(ref) => (
+            <Cart ref={ref} toggleCart={toggleCart} />
+          )}
+        </Transition>
+      )}
+      
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/painting/:id" element={<ImagePicker />}></Route>
-        <Route path="/sign-in" element={<Register />}></Route>
+        <Route path="/painting/:id" element={<ImagePicker />} />
+        <Route path="/sign-in" element={<Register />} />
       </Routes>
       <Footer />
     </BrowserRouter>
